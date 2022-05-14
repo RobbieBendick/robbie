2fb35b4d0782c171ceba5fb43c1684a07ac7331d
Robbie Bendick <robbiebendick@gmail.com>
2022-05-14 09:00:19 -0700

    fix email color transition

--- src/components/SideSocials/SideSocials.jsx
+++ src/components/SideSocials/SideSocials.jsx
@@ -15,7 +15,7 @@ function SideSocials() {
                     <motion.button whileHover={{y: "-2px", color: "#64ffda"}} href="https://github.com/RobbieBendick"><i class="fa-brands fa-github"></i></motion.button>
                 </li>
                 <li>
-                    <motion.button whileHover={{y: "-2px", color: "#64ffda"}} href="https://www.linkedin.com/in/robert-bendick-b71a851b2/"><motion.i onHoverEnd={{backgroundColor: "red"}} class="fa-brands fa-linkedin-in"></motion.i></motion.button>
+                    <motion.button whileHover={{y: "-2px", color: "#64ffda"}} href="https://www.linkedin.com/in/robert-bendick-b71a851b2/"><i class="fa-brands fa-linkedin-in"></i></motion.button>
                 </li>
                 <li>
                     <motion.button whileHover={{y: "-2px", color: "#64ffda"}} href="https://www.linkedin.com/in/robert-bendick-b71a851b2/"><i class="fa-solid fa-envelope"></i></motion.button>
