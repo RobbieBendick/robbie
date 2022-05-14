8fb7ee140f2a44623ab533d1b5649624dc6b300f
Robbie Bendick <robbiebendick@gmail.com>
2022-05-14 08:28:51 -0700

    add framer motion to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,17 @@
 import "./Footer.scss"
+import {motion} from "framer-motion";
 
 function Footer() {
     return (
         <footer className="footer">
             Made by Robert Bendick
             <div className="footer-socials">
-                <li>
-                    <a href="https://github.com/RobbieBendick"><i class="fa-brands fa-github"></i></a>
-                </li>
-                <li>
-                    <a href="https://www.linkedin.com/in/robert-bendick-b71a851b2/"><i class="fa-brands fa-linkedin"></i></a>
-                </li>
+                <div>
+                    <motion.button whileHover={{color: "#64ffda"}} style={{color: "#a8b2d1"}} href="https://github.com/RobbieBendick"><i class="fa-brands fa-github"></i></motion.button>
+                </div>
+                <div>
+                    <motion.button whileHover={{color: "#64ffda"}} style={{color: "#a8b2d1"}} href="https://www.linkedin.com/in/robert-bendick-b71a851b2/"><i class="fa-brands fa-linkedin"></i></motion.button>
+                </div>
             </div>
         </footer>
     )
