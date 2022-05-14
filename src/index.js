afcc91942842c06b1dd60aeaaf7faab2159b1fae
Robbie Bendick <robbiebendick@gmail.com>
2022-05-14 10:02:22 -0700

    lazygit

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Nav />
     <Home />
     <AboutMe />
     <SideSocials />
