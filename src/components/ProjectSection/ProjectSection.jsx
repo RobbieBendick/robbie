00a4d80f1cae03d0866c92a618bb69687d5dd710
Robbie Bendick <robbiebendick@gmail.com>
2022-05-14 15:53:25 -0700

    add project animations

--- src/components/ProjectSection/ProjectSection.jsx
+++ src/components/ProjectSection/ProjectSection.jsx
@@ -1,16 +1,19 @@
 import "./ProjectSection.scss";
 import {React, useEffect} from "react";
+import {motion} from "framer-motion";
 function ProjectCard() {
     return (
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
+        <motion.div whileHover={{y: "-3px"}} className="project-inner">
+            <div whileHover={{y: "-3px"}} class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a>
+                </div>
             </div>
-        </div>
+        </motion.div>
     )
 
 }
@@ -29,24 +32,12 @@ function ProjectSection() {
                 <p className="numbered-heading-projects">Projects</p>
             </div>
             <ul className="projects-grid">
-                <div className="project-inner">
-                    <ProjectCard />
-                </div>
-                <div className="project-inner">
-                    <ProjectCard />
-                </div>
-                <div className="project-inner">
-                    <ProjectCard />
-                </div>
-                <div className="project-inner">
-                    <ProjectCard />
-                </div>
-                <div className="project-inner">
-                    <ProjectCard />
-                </div>
-                <div className="project-inner">
-                    <ProjectCard />
-                </div>
+                <ProjectCard />
+                <ProjectCard />
+                <ProjectCard />
+                <ProjectCard />
+                <ProjectCard />
+                <ProjectCard />
             </ul>
         </section>
     );
