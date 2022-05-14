9ce35f0c0cc697161d7894f59b9445b0aa0816c6
Robbie Bendick <robbiebendick@gmail.com>
2022-05-13 19:21:48 -0700

    update

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,10 +1,17 @@
 import "./Nav.scss"
-import React, {useState} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import useWindowSize from "../../hooks/useWindowSize";
+import $ from "jquery";
+
 
 function Nav() {
+
+
     const windowSize = useWindowSize();
     const [navIsOpen, setNavIsOpen] = useState(false);
+    const sidebarWidth = "250px";
+    let nav = document.getElementById("mySidenav");
+
     let buttons = [];
     if (navIsOpen) {
         buttons.push(<i className="fas fa-minus"></i>);
@@ -14,27 +21,63 @@ function Nav() {
 
     /* Set the width of the side navigation to 250px or 0px */
     function toggleNav() {
-        let nav = document.getElementById("mySidenav");
         if (navIsOpen) {
             setNavIsOpen(false);
             nav.style.width = "0";
         } else {
             setNavIsOpen(true);
-            nav.style.width = "250px";
+            nav.style.width = sidebarWidth;
         }
     }
-    
 
+    let menuRef = useRef();
+    let menuToggleButtonRef = useRef();
+
+    // "click-out" of dropdown
+    useEffect(() => {
+        document.addEventListener('mousedown', event => {
+            if (!menuRef.current.contains(event.target) && !menuToggleButtonRef.current.contains(event.target)) {
+                setNavIsOpen(false);
+                nav.style.width = "0";
+                $(".nav-btn")
+            }
+        })
+    });
     return (
         <>
-        <div id="mySidenav" class="sidenav">
+        <div ref={menuRef} id="mySidenav" class="sidenav">
             <a href="#" class="closebtn"></a>
             <a href="#">About</a>
             <a href="#">Projects</a>
-            <a href="#">Resume</a>
             <a href="#">Contact</a>
+            <a href="#">Resume</a>
         </div>
-        {windowSize.width < 800 ? <span class='nav-btn' onClick={toggleNav}>{buttons}</span> : null}
+         
+        <>
+            <nav class="navbar navbar-light">
+                <a class="navbar-brand" href="#">R</a>
+                <div class="" id="navbarNav">
+                        {windowSize.width < 800 ?
+                        <button class='nav-btn' style={{"zIndex": 33}} ref={menuToggleButtonRef} onClick={toggleNav}>{buttons}</button>
+                        :
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#">About <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Projects</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Contact</a>
+                            </li>
+                            <li class="nav-item">
+                                <button className="resume-button">Resume</button>
+                            </li>   
+                        </ul>
+                        }
+                </div>
+            </nav>
+        </>
         
         <div id="main"></div>
         </>
