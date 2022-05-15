e64b543ee0e3d89b36025975b0285fac4d5d0bb4
Robbie Bendick <robbiebendick@gmail.com>
2022-05-14 22:49:10 -0700

    lazygit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,7 +4,8 @@ import {motion} from "framer-motion";
 function Home() {
 
   return (
-      
+      <>
+
     <div>
         <section className='home'>
             <div>
@@ -17,13 +18,14 @@ function Home() {
                 <h3 className='big-heading'>I build things for the web.</h3>
             </div>
             <div>
-                <p className='about-me-header'>I'm a software engineer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building accessible, human-centered products</p>
+                <p className='about-me-header'>I'm a software engineer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building accessible products which solves real world problems.</p>
             </div>
             <div>
                 <motion.a href="#projects" whileHover={{backgroundColor: "rgb(100, 255, 218, 0.1)"}} className='button-header'>Check out my work! <i class="fa-solid fa-arrow-down" style={{"paddingLeft": "7px", "paddingTop": "50px"}}></i></motion.a>
             </div>
         </section>
     </div>
+    </>
   );
 }
 
