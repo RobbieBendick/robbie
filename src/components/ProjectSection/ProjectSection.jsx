f98f2698a8b1e3c7d30c231f96c41c6b25ea8103
Robbie Bendick <robbiebendick@gmail.com>
2022-05-14 17:34:05 -0700

    update cards

--- src/components/ProjectSection/ProjectSection.jsx
+++ src/components/ProjectSection/ProjectSection.jsx
@@ -1,19 +1,39 @@
 import "./ProjectSection.scss";
 import {React, useEffect} from "react";
 import {motion} from "framer-motion";
-function ProjectCard() {
-    return (
-        <motion.div whileHover={{y: "-3px"}} className="project-inner">
-            <div whileHover={{y: "-3px"}} class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="card-link">Card link</a>
-                    <a href="#" class="card-link">Another link</a>
+
+function ProjectCard({title, description, tech1, tech2, tech3}) {
+        return (
+        <li>
+            <motion.div whileHover={{y: "-3px" }} className="project-inner">
+                <div class="card">
+                    <div class="card-body">
+                        <div className="project-top">
+                            <div className="project-folder">
+                                <a> <i class="fa-regular fa-folder fa-xl"></i> </a>
+                            </div>
+                            <div className="project-links">
+                                <motion.button whileHover={{y: "-2px", color: "#64ffda"}}>
+                                    <i class="fa-brands fa-github fa-sm"></i>
+                                </motion.button>
+                                <motion.button whileHover={{y: "-2px", color: "#64ffda"}}>
+                                <i class="fa-solid fa-arrow-up-right-from-square fa-sm"></i>
+                                </motion.button>
+                            </div>
+                        </div>
+                        <h5 class="card-title">{title}</h5>
+                        <p class="card-text">{description}</p>
+                    </div>
+                    <footer>
+                        <ul className="project-tech-list">
+                            <li>{tech1}</li>
+                            <li>{tech2}</li>
+                            <li>{tech3}</li>
+                        </ul>
+                    </footer>
                 </div>
-            </div>
-        </motion.div>
+            </motion.div>
+        </li>
     )
 
 }
@@ -32,12 +52,12 @@ function ProjectSection() {
                 <p className="numbered-heading-projects">Projects</p>
             </div>
             <ul className="projects-grid">
-                <ProjectCard />
-                <ProjectCard />
-                <ProjectCard />
-                <ProjectCard />
-                <ProjectCard />
-                <ProjectCard />
+                <ProjectCard title="projectName" description="this is a small sample description that needs to be decently long so i can see how it fits" tech1="React" tech2="Node" tech3="Express"/>
+                <ProjectCard title="projectName" description="this is a small sample description that needs to be decently long so i can see how it fits" tech1="React" tech2="Node" tech3="Express"/>
+                <ProjectCard title="projectName" description="this is a small sample description that needs to be decently long so i can see how it fits" tech1="React" tech2="Node" tech3="Express"/>
+                <ProjectCard title="projectName" description="this is a small sample description that needs to be decently long so i can see how it fits" tech1="React" tech2="Node" tech3="Express"/>
+                <ProjectCard title="projectName" description="this is a small sample description that needs to be decently long so i can see how it fits" tech1="React" tech2="Node" tech3="Express"/>
+                <ProjectCard title="projectName" description="this is a small sample description that needs to be decently long so i can see how it fits" tech1="React" tech2="Node" tech3="Express"/>
             </ul>
         </section>
     );
