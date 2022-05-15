62ac6116e5efed695ee88f2888c0a992779ad094
Robbie Bendick <robbiebendick@gmail.com>
2022-05-14 23:31:09 -0700

    add projects

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -9,7 +9,6 @@ function AboutMe() {
             <div>
                 <p>Hello! My name is Rob and I enjoy creating things that live on the internet. My interest in web development started back in 2012 when I decided to try editing custom Tumblr themes — turns out hacking together a custom reblog button taught me a lot about HTML & CSS!</p>
                 <p>Fast-forward to today, and I’ve had the privilege of working at an advertising agency, a start-up, a huge corporation, and a student-led design studio. My main focus these days is building accessible, inclusive products and digital experiences at Upstatement for a variety of clients.</p>
-                <p>I also recently launched a course that covers everything you need to build a web app with the Spotify API using Node & React.</p>
                 <p>Here are a few technologies I’ve been working with recently:</p>
             </div>
             <ul className="skills-list">
@@ -17,10 +16,10 @@ function AboutMe() {
                     JavaScript (ES6+)
                 </li>
                 <li>
-                    Python
+                    React
                 </li>
                 <li>
-                    React
+                    Express
                 </li>
                 <li>
                     Lua
@@ -29,7 +28,7 @@ function AboutMe() {
                     Node
                 </li>
                 <li>
-                    Express
+                    Python
                 </li>
             </ul>
         </section>
