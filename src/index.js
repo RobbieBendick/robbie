4cb673ab4a9a6cfbeea94b827e0f24b1c42ae49d
Robbie Bendick <robbiebendick@gmail.com>
2022-05-17 18:46:52 -0700

    gh pages

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ root.render(
   <BrowserRouter>
     <React.StrictMode>
       <Routes>
-        <Route path="/" element={<Home />}/>
+        <Route path="" element={<Home />}/>
         <Route path="/resume" element={<Resume />} />
       </Routes>
     </React.StrictMode>
