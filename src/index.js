ee227a52cc271d488c433ffe1184d8962d17fdd8
Robbie Bendick <robbiebendick@gmail.com>
2022-05-18 12:40:22 -0700

    lazygit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {
-  BrowserRouter,
   Routes,
   Route,
-  HashRouter
+  HashRouter,
 } from "react-router-dom";
 import './index.scss';
 import Home from './components/Home/Home';
@@ -13,25 +12,18 @@ import Resume from './components/Resume/Resume';
 import reportWebVitals from './reportWebVitals';
 
 
+var env = process.env.NODE_ENV === 'development';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
     <React.StrictMode>
-      <Routes>
-        <Route path="/robbie" element={<Home />}/>
-        <Route path="/robbie/resume" element={<Resume />} />
-        <Route
-              path="*"
-              element={
-                <main style={{ padding: "1rem" }}>
-                  <p>There's nothing here!</p>
-                </main>
-              }
-            />
-      </Routes>
+      <HashRouter basename={env ? "/" : "/robbie"}>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
+      </HashRouter>
     </React.StrictMode>
-  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
