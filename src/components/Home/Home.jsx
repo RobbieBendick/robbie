c3d2147846359d120092d497c843a527a0e82b4a
Robbie Bendick <robbiebendick@gmail.com>
2022-05-18 18:43:55 -0700

    fix homepage button and clean up functions

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,4 @@
 import './Home.scss';
-import { useEffect, useState, useRef } from 'react';
-import {motion} from "framer-motion";
 import AboutMe from '../AboutMe/AboutMe';
 import ProjectSection from '../ProjectSection/ProjectSection';
 import Contact from '../Contact/Contact';
@@ -8,37 +6,16 @@ import SideSocials from '../SideSocials/SideSocials';
 import Nav from '../Nav/Nav';
 import Footer from '../Footer/Footer';
 import FadeInDiv from '../FadeInDiv/FadeInDiv';
+import LandingPage from '../LandingPage/LandingPage';
 
 
 function Home() {
 
-    // use framer motion to animate the homepage
-    // then use intersection viewer later
-
-
   return (
     <>
     <Nav />
-      <div className="main">
-        <div>
-            <section className='home'>
-                <div>
-                    <h1 className="hi fade-in-6" style={{"fontSize": 'clamp(14px,5vw,16px)'}}>Hi, my name is</h1>
-                </div>
-                <div>
-                    <h1 className='big-heading light-slate-color fade-in-7'>Robert Bendick.</h1>
-                </div>
-                <div>
-                    <h3 className='big-heading fade-in-8'>I build things for the web.</h3>
-                </div>
-                <div>
-                    <p className='about-me-header fade-in-9'>I'm a software engineer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building accessible products which solve real world problems.</p>
-                </div>
-                <div>
-                    <motion.a href="#projects" whileHover={{backgroundColor: "rgb(100, 255, 218, 0.1)"}} className='button-header fade-in-10'>Check out my work! <i class="fa-solid fa-arrow-down" style={{"paddingLeft": "7px", "paddingTop": "50px"}}></i></motion.a>
-                </div>
-            </section>
-        </div>
+    <div className="main">
+        <LandingPage />
         <FadeInDiv>
             <AboutMe />
         </FadeInDiv>
@@ -48,7 +25,7 @@ function Home() {
         </FadeInDiv>
         <SideSocials />
     </div>
-    <Footer/>
+    <Footer />
     </>
   );
 }
---
+++ src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,29 @@
+import "./LandingPage.scss";
+import {motion} from "framer-motion";
+
+function LandingPage() {
+
+    return (
+        <div>
+            <section className='home'>
+                <div>
+                    <h1 className="hi fade-in-6" style={{"fontSize": 'clamp(14px,5vw,16px)'}}>Hi, my name is</h1>
+                </div>
+                <div>
+                    <h1 className='big-heading light-slate-color fade-in-7'>Robert Bendick.</h1>
+                </div>
+                <div>
+                    <h3 className='big-heading fade-in-8'>I build things for the web.</h3>
+                </div>
+                <div>
+                    <p className='about-me-header fade-in-9'>I'm a software engineer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building accessible products which solve real world problems.</p>
+                </div>
+                <div>
+                    <motion.a onClick={() => document.querySelector("#projects").scrollIntoView({ behavior: "smooth" })} whileHover={{backgroundColor: "rgb(100, 255, 218, 0.1)"}} className='button-header fade-in-10'>Check out my work! <i class="fa-solid fa-arrow-down" style={{"paddingLeft": "7px", "paddingTop": "50px"}}></i></motion.a>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default LandingPage;
