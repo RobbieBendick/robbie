cf509dcab02c57f71cf9c0c5fa0e595118ce4786
Robbie Bendick <robbiebendick@gmail.com>
2022-05-18 18:26:23 -0700

    fix react router with gh-pages

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -137,7 +137,7 @@ function NavAnchorItem({txt, delay}) {
                 <SidebarAnchorItem txt="About" href="#about"/>
                 <SidebarAnchorItem txt="Projects" href="#projects"/>
                 <SidebarAnchorItem txt="Contact" href="#contact"/>
-                <motion.a href={``} target="_blank" whileHover={{backgroundColor: "hsl(166, 100%, 70% / 0.1)"}} className="sidebar-resume-button" rel="noopener noreferrer">Resume</motion.a>
+                <motion.a href={`${process.env.PUBLIC_URL}/#/resume`} target="_blank" whileHover={{backgroundColor: "hsl(166, 100%, 70% / 0.1)"}} className="sidebar-resume-button" rel="noopener noreferrer">Resume</motion.a>
             </aside>
          
             <nav id="navbar" class="navbar navbar-light sticky">
