3fc8015e694cfd13c0c64006d2ff8ea00bd068ec
Robbie Bendick <robbiebendick@gmail.com>
2022-05-18 18:19:05 -0700

    lazygit

--- src/index.js
+++ src/index.js
@@ -1,26 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes} from "react-router-dom";
 import './index.scss';
 import Home from './components/Home/Home';
 import Resume from './components/Resume/Resume';
 import reportWebVitals from './reportWebVitals';
 
 
-var env = process.env.NODE_ENV === 'development';
-console.log(process.env.PUBLIC_URL);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+root.render(  
   <Router>
-    <React.StrictMode>
-      <Routes>
-        <Route path={env ? `/` : `${process.env.PUBLIC_URL}/`} element={<Home />}/>
-        <Route path={env ? `/resume` : `${process.env.PUBLIC_URL}/resume`} element={<Resume />} />
-      </Routes>
-    </React.StrictMode>
+    <Routes>
+      <Route path='/' exact element={<Home />}/>
+      <Route path='/resume' exact element={<Resume />} />
+    </Routes>
   </Router>
-
 );
 
 
