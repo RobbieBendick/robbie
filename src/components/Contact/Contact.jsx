4147efc2db96c23aed50b90240ed5e9325a57b12
Robbie Bendick <robbiebendick@gmail.com>
2022-06-13 15:44:53 -0700

    add keyboard accessibility animations to footer

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -11,7 +11,7 @@ function Contact() {
                 <h2 className="whats-next">What's Next?</h2>
                 <h2 className="get-in-touch">Get In Touch</h2>
                 <p className="contact-description">I'm currently looking for more opportunities and my inbox is always open. Whether you have a question or just want to say hi, I’ll try my best to get back to you!</p>
-                <motion.a href="mailto:robbiebendick@gmail.com" whileHover={{backgroundColor: "rgb(100, 255, 218, 0.1)", color: "#64ffda"}} className="contact-button">Say Hello</motion.a>
+                <motion.a href="mailto:robbiebendick@gmail.com" whileFocus={{backgroundColor: "rgb(100, 255, 218, 0.1)"}} whileHover={{backgroundColor: "rgb(100, 255, 218, 0.1)"}} className="contact-button">Say Hello</motion.a>
             </section>
         </FadeInDiv>
     )
