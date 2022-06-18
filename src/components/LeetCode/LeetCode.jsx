5c2a65cfe7eb7e9e2324af0a6cb721ae9c0709a6
Robbie Bendick <robbiebendick@gmail.com>
2022-06-17 20:57:31 -0700

    update leetcode section

--- src/components/LeetCode/LeetCode.jsx
+++ src/components/LeetCode/LeetCode.jsx
@@ -11,16 +11,19 @@ import SyntaxHighlighter from 'react-syntax-highlighter';
 let LeetCode = () => {
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
+    const [modalProblem, setModalProblem] = useState("");
 
-    const handleShow = (title, body, timeComplexity) => {
+    const handleShow = (title, body, problemDescription, problem, solutionDescription, timeComplexity, difficulty) => {
+        setProblemDescription(problemDescription);
+        setSolutionDescription(solutionDescription)
+        setModalDifficulty(difficulty)
+        setModalProblem(problem);
         setModalHeader(title);
-        setModalBody(body)
-        setModalTimeComplexity(timeComplexity)
+        setModalBody(body);
+        setModalTimeComplexity(timeComplexity);
         setShow(true);
     };
-    function MyModal() {
-
-
+    let MyModal = () => {
         let navyShadow = "hsl(216, 86%, 6%, 0.7)";
         let slate = 'hsl(225, 20%, 61%)';
         let lightestSlate = 'hsl(226, 70%, 88%)';
@@ -28,15 +31,21 @@ let LeetCode = () => {
         return (
               <>
                 <Modal show={show} onHide={handleClose} style={{width: "100%",backgroundColor: 'hsl(216, 65%, 11%)',  border: 'none', boxShadow: `0 10px 30px -15px ${navyShadow}`}}>
-                  <Modal.Header closeButton style={{ backgroundColor: 'hsl(218, 41%, 23%)', border: 'none'}}>
-                    <Modal.Title style={{ backgroundColor: 'hsl(218, 41%, 23%)', border: 'none', color: lightestSlate}}>{modalHeader}</Modal.Title>
+                <Modal.Header  closeButton style={{ backgroundColor: 'hsl(218, 41%, 23%)', border: 'none'}}>
+                    <Modal.Title style={{ backgroundColor: 'hsl(218, 41%, 23%)', border: 'none', color: lightestSlate, display: 'flex'}}>{modalHeader}</Modal.Title>
+                    <Modal.Title className={modalDifficulty.toLowerCase()}>{modalDifficulty}</Modal.Title>
                   </Modal.Header>
+                <Modal.Body style={{ backgroundColor: 'hsl(218, 41%, 23%)', border: 'none', color: slate}}>{problemDescription}</Modal.Body>
+                <Modal.Body style={{ backgroundColor: 'hsl(218, 41%, 23%)', border: 'none', color: slate}}>{modalProblem}</Modal.Body>
+                <Modal.Header  style={{ backgroundColor: 'hsl(218, 41%, 23%)', border: 'none'}}>
+                    <Modal.Title style={{ backgroundColor: 'hsl(218, 41%, 23%)', border: 'none', color: lightestSlate}}>{"Solution"}</Modal.Title>
+                </Modal.Header>
                   <Modal.Body style={{ backgroundColor: 'hsl(218, 41%, 23%)', border: 'none', color: slate}}>{modalBody}</Modal.Body>
                   <Modal.Footer style={lightestNavy}>
-                  <p>Time Complexity: {modalTimeComplexity}</p>
-                    <Button style={lightestNavy} onClick={handleClose}>
-                      Close
-                    </Button>
+                    <p>{solutionDescription}</p>
+                  </Modal.Footer>
+                  <Modal.Footer style={lightestNavy}>
+                    <p>Time Complexity: {modalTimeComplexity}</p>
                   </Modal.Footer>
                 </Modal>
               </>
@@ -46,16 +55,19 @@ let LeetCode = () => {
     const [modalBody, setModalBody] = useState("");
     const [modalHeader, setModalHeader] = useState("");
     const [modalTimeComplexity, setModalTimeComplexity] = useState("");
+    const [problemDescription, setProblemDescription] = useState("");
+    const [solutionDescription, setSolutionDescription] = useState("");
+    const [modalDifficulty, setModalDifficulty] = useState("");
 
 
     let Table = () => {
 
-        let tableBodyRow = (title, difficulty, language, funcParam1, timeComplexity) => {
+        let tableBodyRow = (title, difficulty, language, problemDescription, problem, solution, solutionDescription, timeComplexity) => {
             return (
             <tr>
                 <td>{title}</td>
                 <td><button className={difficulty.toLowerCase()}>{difficulty}</button></td>
-                <td><button onClick={() => handleShow(title, funcParam1, timeComplexity)}>Code Explanation</button></td>
+                <td><button onClick={() => handleShow(title, solution, problemDescription, problem, solutionDescription, timeComplexity, difficulty)}>Explanation</button></td>
                 <td>{language}</td>
             </tr>
             )
@@ -67,16 +79,33 @@ let LeetCode = () => {
             "title": "Find First and Last Position of Element in Sorted Array",
             "difficulty": "Medium",
             "language": "JavaScript",
+            "problemDescription": "Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value. If target is not found in the array, return [-1, -1].",
+            "problem": <SyntaxHighlighter language="javascript">{`Example 1: Input: nums = [5,7,7,8,8,10], target = 8;\n           Output: [3,4];\nExample 2: Input: nums = [5,7,7,8,8,10], target = 6;\n           Output: [-1,-1];\nExample 3: Input: nums = [], target = 0;\n           Output: [-1,-1];\n`}</SyntaxHighlighter>,
+            "solutionDescription": "Solution Description",
             "body": <SyntaxHighlighter language="javascript">{
-                `let searchRange = (nums, target) => {
-                    let first = -1;
-                    let last = -1;
-                    for(i=0;i<nums.length;i++){
-                        if(nums[i] === target && first === -1) first = i;
-                        if(nums[i] === target && first !== -1 && nums[i+1] !== target) last = i;
-                    }
-                    return [first, last];
-                };`}
+`let searchRange = (nums, target) => {
+    let first = -1;
+    let last = -1;
+    let j = nums.length;
+
+    // if array is empty or has 1 element
+    if (nums.length === 0) return [first,last];
+    if (nums.length === 1) return nums[0] === target ? [0, 0] : [first, last];
+
+    // traverse through list from both ways
+    for(i=0;i<nums.length;i++){
+        if(nums[i] === target && first === -1) first = i;
+        if(nums[j] === target && last === -1) last = j;
+        j--;
+    }
+                
+    // if there's only one target in the array
+    if (first === -1 && last !== -1) first = last;
+    if (last === -1 && first !== -1) last = first;
+
+    // return array of first and last indices.
+    return [first, last];
+};`}
             </SyntaxHighlighter>,
             "timeComplexity": "O(n)",
         },
@@ -88,7 +117,7 @@ let LeetCode = () => {
                 for (i=0; i<nums.length; i++) {
                     for(j=0;j<nums.length;j++) {
                         if (Math.abs(nums[i] - nums[j]) <= t &&
-                        Math.abs(i - j)<= k && i !== j)
+                        Math.abs(i - j) <= k && i !== j)
                         return true
                     } 
                 }
@@ -101,6 +130,11 @@ let LeetCode = () => {
             "title": "Contains Duplicate II", 
             "difficulty": "Easy",
             "language": "JavaScript",
+            "problemDescription": "Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value. If target is not found in the array, return [-1, -1].",
+            "problem": <SyntaxHighlighter language="javascript">{`Example 1 Input: nums = [5,7,7,8,8,10], target = 8; \n          Output: [3,4]; \n
+Example 2 Input: nums = [5,7,7,8,8,10], target = 6;\n          Output: [-1,-1];\n        
+Example 3 Input: nums = [], target = 0;\n          Output: [-1,-1]`}</SyntaxHighlighter>,
+            "solutionDescription": "Solution Description",
             "body": <SyntaxHighlighter language="javascript">{`let containsNearbyAlmostDuplicate = (nums, k, t) => {
                 for (i=0; i<nums.length; i++) {
                     for(j=0;j<nums.length;j++) {
@@ -118,6 +152,8 @@ let LeetCode = () => {
             "title": "Unique Occurences",
             "difficulty": "Easy",
             "language": "JavaScript",
+            "problemDescription": "Given an array of integers arr, return true if the number of occurrences of each value in the array is unique, or false otherwise.",
+            "problem": <SyntaxHighlighter language="javascript">{`Example 1: Input: arr = [1,2,2,1,1,3];\n           Output: true;\n           Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1.\n           No two values have the same number of occurrences.\nExample 2: Input: arr = [1,2];\n           Output: false;\nExample 3: Input: arr = [-3,0,1,-3,1,1,1,-3,10,0];\n           Output: true;\n`}</SyntaxHighlighter>,
             "body": <SyntaxHighlighter language="javascript">{
                 `let uniqueOccurrences = arr => {
                 let map = new Map();
@@ -158,7 +194,7 @@ let LeetCode = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            {tableRows.map(v => tableBodyRow(v.title, v.difficulty, v.language, v.body, v.timeComplexity))}
+                            {tableRows.map(v => tableBodyRow(v.title, v.difficulty, v.language, v.problemDescription,v.problem, v.body, v.solutionDescription, v.timeComplexity))}
                         </tbody>
                     </table>
             <MyModal />
