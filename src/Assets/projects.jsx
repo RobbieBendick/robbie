a0719512b7a2b343ea0f1c664fd2fda4773889b3
Robbie Bendick <robbiebendick@gmail.com>
2023-06-07 10:49:31 -0700

    update projs

---
+++ src/Assets/projects.jsx
@@ -0,0 +1,84 @@
+const cardDetails = [
+  {
+    title: 'ArenaMarker',
+    description:
+      'Fully customizable addon that automates tedious UI tasks. 33k+ downloads and currently rank 14 in popularity, among other addons in its respective category.',
+    githubSrc: 'https://github.com/RobbieBendick/ArenaMarker',
+    techList: ['Lua'],
+    techTag: 'Lua',
+  },
+  {
+    title: 'CurbCompanion',
+    description:
+      'Full stack web app that connects users with local food trucks/vendors. Menu, Schedule & Location features.',
+    externalSrc: null,
+    techList: ['TS', 'Express', 'MongoDB', 'Flutter'],
+    techTag: 'JS Node Flutter',
+  },
+  {
+    title: 'A* Pathfinding Algorithm',
+    description:
+      'Calculates and visualizes the fastest route from the starting location to the end location while maneuvering around barricades.',
+    githubSrc: 'https://github.com/RobbieBendick/a-star-pathfinding',
+    techList: ['Python'],
+    techTag: 'Python',
+  },
+  {
+    title: 'GladiatorGuru',
+    description:
+      'A full stack we app that provides World of Warcraft players with a platform to connect with professional players for coaching or gaming.',
+    externalSrc: 'https://gladiatorguru.com/',
+    techList: ['React', 'JS', 'Express', 'MongoDB'],
+    techTag: 'JS Node',
+  },
+  {
+    title: 'Dark Theme',
+    description:
+      'Provides a Dark Theme as part of a customizable UI Addon/Plugin written in Lua.',
+    githubSrc: 'https://github.com/RobbieBendick/DarkTheme',
+    techList: ['Lua'],
+    techTag: 'Lua',
+  },
+  {
+    title: 'Anon',
+    description:
+      "Plugin/AddOn that color-coordinates unit nameplates, and disguises player and player companion's names.",
+    githubSrc: 'https://github.com/RobbieBendick/Anon',
+    techList: ['Lua'],
+    techTag: 'Lua',
+  },
+  {
+    title: 'TotemPredictor',
+    description:
+      'Plugin/AddOn that predicts the best totem to use in a given situation.',
+    githubSrc: 'https://github.com/RobbieBendick/TotemPredictor',
+    techList: ['Lua'],
+    techTag: 'Lua',
+  },
+  {
+    title: 'Google Keep Clone',
+    description: 'A simple Google Keep clone built with React.',
+    githubSrc: 'https://github.com/RobbieBendick/note-keeper',
+    externalSrc: 'https://robbiebendick.github.io/note-keeper/',
+    techList: ['JS', 'React', 'CSS'],
+    techTag: 'JS React CSS',
+  },
+  {
+    title: 'Simon Clone',
+    description: "Web clone of the game 'Simon'.",
+    githubSrc: 'https://github.com/RobbieBendick/memorizing-game',
+    externalSrc: 'https://robbiebendick.github.io/memorizing-game/',
+    techList: ['JS', 'JQuery', 'CSS', 'HTML'],
+    techTag: 'JS CSS',
+  },
+  {
+    title: 'PartyFrames',
+    description:
+      'Plugin/AddOn to change sizes and colors of default game party frames.',
+    githubSrc: 'https://github.com/RobbieBendick/PartyFrames',
+    techList: ['Lua'],
+    techTag: 'Lua',
+  },
+];
+// all project cards
+export default cardDetails;
